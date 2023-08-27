4858c41d04c7eca3e9a7bd9efe111037b8c9c8f2
Kumar Anurag <kan@Kumars-MacBook-Air.local>
2023-08-27 12:11:55 +0530

    page dimensions fixed

--- 13_QRi.Py/main.py
+++ 13_QRi.Py/main.py
@@ -4,16 +4,20 @@ import cv2
 
 def main(page: ft.Page):
     page.title = "QR Code Scanner and Generator"
-    page.horizontal_alignment = 'center'
-    page.vertical_alignment = 'center'
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window_height = 550
+    page.window_width = 750
     page.theme_mode = 'light'
     page.scroll = 'always'
     page.bgcolor = '#ffffff'
     page.window_bgcolor = '#522125'
 
+    qr_to_show = ft.Image(width=200)
+
     dlg = ft.AlertDialog(
         title=ft.Text('Scan QR Code',text_align='center'),
-        content=ft.Image(src='qr/qrCode.jpg', width=200)
+        content=qr_to_show
     )
 
     def open_dlg(e):
@@ -24,6 +28,7 @@ def main(page: ft.Page):
     def generate_qrCode(e):
         img = qrcode.make(str(user_content.value))
         img.save('assets/qr/qrCode.jpg')
+        qr_to_show.src = 'qr/qrCode.jpg'
         open_dlg(e)
         page.update()
     
@@ -34,7 +39,6 @@ def main(page: ft.Page):
         show_qr_content.value = value
         page.update()
 
-
     user_content = ft.TextField(hint_text='please put the content to encrypt', border_radius=40, border_color='#522125',color='#522125')
     generate_button = ft.ElevatedButton("Generate", on_click=generate_qrCode,bgcolor='#522125',color=ft.colors.WHITE)
 
@@ -43,8 +47,8 @@ def main(page: ft.Page):
 
     page.add(
         ft.Row([
-            ft.Column(),
             ft.Column([ft.Image(src='qrCodeCover.png', height=500)],horizontal_alignment='center',alignment='center'),
+            ft.Container(width=30),
             ft.Column([
                 ft.Image(src='logo/appLogo.png',width=150),
                 ft.Container(height=20),
@@ -56,8 +60,8 @@ def main(page: ft.Page):
                 ft.Text(value='Made by: Kumar Anurag',color='#522125',weight='bold')
             ],horizontal_alignment='center',alignment='center'),
         ],
-        alignment='center',
-        vertical_alignment='center'
+        vertical_alignment='center',
+        alignment='center'
         ),
     )
 
